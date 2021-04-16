58c87a3ba907bc1170648a8115a907dc880cfa09
Nick Knapton <nickknapton12@gmail.com>
2021-04-15 23:31:33 -0600

    added animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './styles/App.css';
-import LandingPage from './pages/LandingPage.js';
-import React from 'react';
+import "./styles/App.css";
+import LandingPage from "./pages/LandingPage.js";
+import React from "react";
 
 class App extends React.Component {
 
@@ -8,31 +8,61 @@ class App extends React.Component {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
   }
 
+  showMenu(){
+    console.log("menu");
+    document.querySelector('.navBar').classList.toggle('nav-active');
+    document.querySelector('.menu').classList.toggle('nav-active');
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onScroll={this.scrollDown}>
         <div id="page1">
           <div className="backgroundNav">
             <div className="header">
               <h1 className="pageHeader">Nicholas Knapton</h1>
-              <div></div>
-              <div className="menu">
+              <div className="menu" onClick={this.showMenu}>
                 <h1 className="menuHeader">
-                  <div className="menuCircle">
-                  </div> Menu
+                  <div className="menuCircle"></div> Menu
                 </h1>
               </div>
-            </div>
+              <div className="navBar">
 
+            </div>
+            </div>
           </div>
           <LandingPage />
-          <i class="material-icons" id="downIcon" onClick={() => this.scrollDown("page2")}>keyboard_arrow_down</i>
+          <i
+            className="material-icons"
+            id="downIcon"
+            onClick={() => this.scrollDown("page2")}
+          >
+            keyboard_arrow_down
+          </i>
         </div>
         <div id="page2">
-          <i class="material-icons" id="upIcon" onClick={() => this.scrollDown("page1")}>keyboard_arrow_up</i>
-          <h1 className="nextPageButton" onClick={() => this.scrollUp("page3")}> &#62; </h1>
+          <i
+            class="material-icons"
+            id="upIcon"
+            onClick={() => this.scrollDown("page1")}
+          >
+            keyboard_arrow_up
+          </i>
+          <h1
+            className="nextPageButton"
+            onClick={() => this.scrolldown("page3")}
+          >
+            {" "}
+            &#62;{" "}
+          </h1>
           <h1 className="pageHeader">P2</h1>
-          <i class="material-icons" id="downIcon" onClick={() => this.scrollDown("page2")}>keyboard_arrow_down</i>
+          <i
+            class="material-icons"
+            id="downIcon"
+            onClick={() => this.scrollDown("page2")}
+          >
+            keyboard_arrow_down
+          </i>
         </div>
       </div>
     );
